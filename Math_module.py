170b05238777f46bc188447d43ffdb1b8bea14f0
AravitiManoj <75194272+AravitiManoj@users.noreply.github.com>
2026-02-05 05:40:08 +0530

    Update and rename RangeSum.py to Math_module.py

---
+++ Math_module.py
@@ -0,0 +1,6 @@
+import math
+
+mathinput = int(input("Enter a number: "))
+print(f"Square root: ",math.sqrt(mathinput))
+print(f"Logarithm: ", math.log(mathinput))
+print(f"Sine: ",math.sin(mathinput))
